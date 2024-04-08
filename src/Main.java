4680427857e9a9c26a26b2dc3a3e695c645bd2af
Barhatova <barhatov4.i@yandex.ru>
2024-04-08 21:21:58 +0300

    4 спринт "Трекер задач"

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,48 @@
+import model.Epic;
+import model.Status;
+import model.SubTask;
+import model.Task;
+import service.TaskManager;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Поехали!");
+
+        TaskManager taskManager = new TaskManager();
+
+        Task task1 = taskManager.createTask(new Task("Переезд", "Купить дом", Status.IN_PROGRESS));
+        Task task2 = taskManager.createTask(new Task("Приготовить обед", "Купить продукты", Status.NEW));
+        System.out.println("createTask:" + task1);
+        System.out.println("createTask:" + task2);
+
+        Epic epic1 = taskManager.createEpic(new Epic("Сделать ремонт", "Ремонт новой квартиры",
+                Status.NEW));
+        SubTask subTask1 = taskManager.createSubtask(new SubTask("Купить материалы", "Стройматериалы",
+                Status.DONE, epic1));
+        SubTask subTask2 = taskManager.createSubtask(new SubTask("Нанять рабочих", "Плиткоукладчики",
+                Status.DONE, epic1));
+        epic1.addSubTask(subTask1);
+        epic1.addSubTask(subTask2);
+        System.out.println("createEpic:" + epic1);
+
+        Epic epic2 = taskManager.createEpic(new Epic("Испечь торт", "Торт для семьи", Status.NEW));
+        SubTask subTask3 = taskManager.createSubtask(new SubTask("найти рецепт", "Торт-Птичье молоко",
+                Status.NEW, epic2));
+        epic2.addSubTask(subTask3);
+        System.out.println("createEpic:" + epic2);
+
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllSubTasks());
+        System.out.println(taskManager.getAllEpics());
+
+        taskManager.deleteTaskById(1);
+        taskManager.deleteEpicById(6);
+
+
+        System.out.println(taskManager.getAllEpics());
+        System.out.println(taskManager.getAllTasks());
+        System.out.println(taskManager.getAllSubTasks());
+
+        System.out.println(taskManager.calculateEpicStatus());
     }
 }
