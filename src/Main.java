62862c7c4853ee0713e4e3b495e1a2638d527231
Ирина <barhatov4@yandex.ru>
2025-02-22 17:02:18 +0300

    5 спринт

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,74 @@
-import exception.ValidationException;
+import model.*;
 import service.*;
+import java.util.List;
 
 public class Main {
 
-    public static void main(String[] args) throws ValidationException {
+    public static void main(String[] args) {
 
-        TaskManager manager = Managers.getDefault();
+        TaskManager taskManager = Managers.getDefault();
+
+        Task task1 = taskManager.createTask(new Task("Переезд", "Купить дом", Status.NEW));
+
+        Task task2 = taskManager.createTask(new Task("Приготовить обед", "Купить продукты", Status.NEW));
+
+        Epic epic1 = taskManager.createEpic(new Epic("Сделать ремонт", "Ремонт новой квартиры",
+                Status.NEW));
+        SubTask subTask1 = taskManager.createSubtask(new SubTask("Купить материалы", "Стройматериалы",
+                Status.DONE, epic1,2));
+        SubTask subTask2 = taskManager.createSubtask(new SubTask("Нанять рабочих", "Плиткоукладчики",
+                Status.NEW, epic1, 2));
+        epic1.addSubTask(subTask1);
+        epic1.addSubTask(subTask2);
+
+        Epic epic2 = taskManager.createEpic(new Epic("Испечь торт", "Торт для семьи", Status.NEW));
+        SubTask subTask3 = taskManager.createSubtask(new SubTask("Найти рецепт", "Торт-Птичье молоко",
+                Status.NEW, epic2,5));
+        epic2.addSubTask(subTask3);
+
+        List<Task> historyBrowsing = taskManager.getHistory();
+
+        System.out.println("История:");
+
+        taskManager.getTaskById(0);
+        System.out.println(historyBrowsing.get(0));
+
+        taskManager.getTaskById(1);
+        System.out.println(historyBrowsing.get(1));
+
+        taskManager.getEpicById(2);
+        System.out.println(historyBrowsing.get(2));
+
+        taskManager.getSubtaskById(3);
+        System.out.println(historyBrowsing.get(3));
+
+        taskManager.getSubtaskById(4);
+        System.out.println(historyBrowsing.get(4));
+
+        taskManager.getEpicById(5);
+        System.out.println(historyBrowsing.get(5));
+
+        taskManager.getSubtaskById(6);
+        System.out.println(historyBrowsing.get(6));
+
+        taskManager.getTaskById(0);
+        System.out.println(historyBrowsing.get(7));
+
+        taskManager.getTaskById(1);
+        System.out.println(historyBrowsing.get(8));
+
+        taskManager.getEpicById(2);
+        System.out.println(historyBrowsing.get(9));
 
     }
 }
+
+
+
+
+
+
+
 
 
 
--- src/service/InMemoryHistoryManager.java
+++ src/service/InMemoryHistoryManager.java
@@ -1,78 +1,26 @@
 package service;
 import model.Task;
-import java.util.*;
+import java.util.List;
+import java.util.LinkedList ;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    Map<Integer, Node> historyBrowsing = new HashMap<>();
-    Node head;
-    Node tail;
-
-    private static class Node {
-        Node prev;
-        Task item;
-        Node next;
-
-        Node(Node prev, Task item, Node next) {
-            this.prev = prev;
-            this.item = item;
-            this.next = next;
-        }
-    }
+    List<Task> historyBrowsing = new LinkedList<>();
+    static final int MAX_VALUE = 10;
 
     @Override
     public void add(Task task) {
-        Node node = historyBrowsing.get(task.getId());
-        if (node != null) {
-            removeNode(node);
-        }
-        linkLast(task);
-    }
-
-    @Override
-    public void remove(int id) {
-        Node node = historyBrowsing.get(id);
-        if (node != null) {
-            removeNode(node);
-            historyBrowsing.remove(id);
+    if (task != null) {
+            if (historyBrowsing.size() == MAX_VALUE) {
+                historyBrowsing.removeFirst();
+            }
+        historyBrowsing.add(task);
         }
     }
 
-    void linkLast(Task task) {
-        Node oldTail = tail;
-        Node newNode = new Node(head, task, oldTail);
-        tail = newNode;
-        if (oldTail == null) {
-            head = newNode;
-        } else {
-            oldTail.prev = newNode;
-        }
-        historyBrowsing.put(task.getId(), newNode);
-    }
-
-    private void removeNode(Node node) {
-        if (node.next == null && node.prev == null) {
-            head = null;
-            tail = null;
-        } else if (node == head) {
-            head = node.next;
-            node.next.prev = null;
-        } else if (node == tail) {
-            tail = node.prev;
-            node.prev.next = null;
-        } else if (node.prev != null && node.next != null) {
-            node.prev.next = node.next;
-        }
-    }
 
     @Override
     public List<Task> getHistory() {
-        List<Task> history = new LinkedList<>();
-        Node current = head;
-        while (current != null) {
-            history.add(current.item);
-            current = current.next;
-        }
-        return history;
+        return historyBrowsing;
     }
 }
